fa62bfdecfc3a45c3eebb07d73dc61335e8043ca
nakwafurkhan <furkhan912@gmail.com>
2024-12-28 22:25:27 +0530

    minor changes

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ canvas.height = window.innerHeight - canvasOffsetY;
 
 let isPainting = false;
 let lineWidth = 5;
-let startX;
-let startY;
 
 toolbar.addEventListener('click', e => {
     if (e.target.id === 'clear') {
@@ -20,39 +18,54 @@ toolbar.addEventListener('click', e => {
 });
 
 toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
+    if (e.target.id === 'stroke') {
         ctx.strokeStyle = e.target.value;
     }
 
-    if(e.target.id === 'lineWidth') {
+    if (e.target.id === 'lineWidth') {
         lineWidth = e.target.value;
     }
-    
 });
 
+const startPainting = (e) => {
+    isPainting = true;
+    draw(e); // Start drawing immediately on touch/mousedown
+};
+
+const stopPainting = () => {
+    isPainting = false;
+    ctx.beginPath();
+};
+
 const draw = (e) => {
-    if(!isPainting) {
-        return;
+    if (!isPainting) return;
+
+    let x, y;
+
+    if (e.type.startsWith('touch')) {
+        const touch = e.touches[0];
+        x = touch.clientX - canvasOffsetX;
+        y = touch.clientY - canvasOffsetY;
+    } else {
+        x = e.clientX - canvasOffsetX;
+        y = e.clientY - canvasOffsetY;
     }
 
     ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
 
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-    ctx.stroke();
-}
-
-canvas.addEventListener('mousedown', (e) => {
-    isPainting = true;
-    startX = e.clientX;
-    startY = e.clientY;
-});
-
-canvas.addEventListener('mouseup', e => {
-    isPainting = false;
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
-});
+    ctx.moveTo(x, y);
+};
 
+// Mouse events
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', stopPainting);
 canvas.addEventListener('mousemove', draw);
-    
+
+// Touch events
+canvas.addEventListener('touchstart', startPainting);
+canvas.addEventListener('touchend', stopPainting);
+canvas.addEventListener('touchmove', draw);
